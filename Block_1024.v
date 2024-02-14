f19464cc8d14dbd3b66e43aec0f8cd9339c0437a
xu-jiayao <61353053+xu-jiayao@users.noreply.github.com>
2024-02-14 16:43:31 +0900

    Update Block_1024.v

--- Block_1024.v
+++ Block_1024.v
@@ -1,3 +1,8 @@
+//Matrices used in this work:
+//sensing matrix: measurement matrix * transform matrix
+//measurement matrix: Walsh Matrix
+//transform matrix: Inversed Walsh-Hadamard Transform Matrix
+
 module Block_1024
 (
 	input clk,

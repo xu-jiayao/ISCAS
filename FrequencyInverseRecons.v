83f420c61f26cc32cb5fdf912aa5b6842e7d7217
xu-jiayao <61353053+xu-jiayao@users.noreply.github.com>
2024-02-14 16:43:10 +0900

    Update FrequencyInverseRecons.v

--- FrequencyInverseRecons.v
+++ FrequencyInverseRecons.v
@@ -1,3 +1,8 @@
+//Matrices used in this work:
+//sensing matrix: measurement matrix * transform matrix
+//measurement matrix: Walsh Matrix
+//transform matrix: Inversed Walsh-Hadamard Transform Matrix
+
 `include "StaticDefines.v"
 module FrequencyInverseRecons
 (	input clk,
